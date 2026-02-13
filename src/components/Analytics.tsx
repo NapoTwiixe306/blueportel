2d5d6d2175c414422d12b8be4e96de9b2c8e9527
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2026-02-13 12:08:35 +0100

    Update Analytics.tsx

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -2,11 +2,26 @@ import Script from "next/script";
 
 export default function Analytics() {
   return (
-    <Script
-      defer
-      data-website-id="3348cf1c-a01f-42c9-9702-3880314be312"
-      src="https://stats.blueportel.com/script.js"
-    />
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-S1N8FDZEDJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-S1N8FDZEDJ');
+        `}
+      </Script>
+      <Script
+        defer
+        data-website-id="3348cf1c-a01f-42c9-9702-3880314be312"
+        src="https://stats.blueportel.com/script.js"
+      />
+    </>
   );
 }
 
